8f8533e5ebcb350b59ee340c6d735fdab75945ed
peolic <66393006+peolic@users.noreply.github.com>
2022-05-24 00:04:05 +0300

    Update 1passforallsites-big-episode-image.user.js

--- 1passforallsites-big-episode-image.user.js
+++ 1passforallsites-big-episode-image.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        1 Pass For All Sites - Better Episode Image
 // @author      peolic
-// @version     1.0
+// @version     1.1
 // @description Attempt to grab a better episode image.
 // @icon        https://1passforallsites.com/media/favicon/favicon-32x32.png
 // @namespace   https://github.com/peolic
@@ -12,16 +12,21 @@
 // @updateURL   https://raw.githubusercontent.com/peolic/userscripts/main/1passforallsites-big-episode-image.user.js
 // ==/UserScript==
 
+//@ts-check
 (() => {
-  const target = document.querySelector('.movie-wrapper > img');
+  const target = /** @type {HTMLImageElement} */ (document.querySelector('.movie-wrapper > img'));
   if (target?.src.endsWith('/movie_tn.jpg')) {
-    const currentScene = window.location.href.match(/(^.+\/\d+\/?)/)[1];
-    const thumbLink = document.querySelector(`a[href^="${currentScene}"`);
+    const currentEpisode = window.location.href.match(/(^.+\/\d+\/?)/)[1];
+    const thumbLink = /** @type {HTMLAnchorElement} */ (document.querySelector(`a[href^="${currentEpisode}"`));
     const thumbSrc = thumbLink.querySelector('img').src;
     const bigSrc = thumbSrc.replace('mainthumb.jpg', 'big.jpg');
 
-    target.addEventListener('error', () => {
-      target.src = thumbSrc;
+    const useThumb = () => target.src = thumbSrc;
+
+    target.addEventListener('error', useThumb);
+    target.addEventListener('load', () => {
+      if (target.src === bigSrc && (target.naturalHeight - target.naturalWidth) > 10)
+        useThumb();
     });
 
     target.src = bigSrc;
