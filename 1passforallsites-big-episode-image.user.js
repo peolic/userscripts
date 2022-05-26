eba301a534d7306acd1c98abfb91874ed11c64fb
peolic <66393006+peolic@users.noreply.github.com>
2022-05-26 18:10:42 +0300

    Update 1passforallsites-big-episode-image.user.js
    break out of loop when thumb is broken

--- 1passforallsites-big-episode-image.user.js
+++ 1passforallsites-big-episode-image.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        1 Pass For All Sites - Better Episode Image
 // @author      peolic
-// @version     1.2
+// @version     1.3
 // @description Attempt to grab a better episode image.
 // @icon        https://1passforallsites.com/media/favicon/favicon-32x32.png
 // @namespace   https://github.com/peolic
@@ -16,6 +16,7 @@
 (() => {
   const target = /** @type {HTMLImageElement} */ (document.querySelector('.movie-wrapper > img'));
   if (target?.src.endsWith('/movie_tn.jpg')) {
+    const originalSrc = target.src;
     const currentEpisode = window.location.href.match(/(^.+\/\d+\/?)/)[1];
     const thumbLink = /** @type {HTMLAnchorElement} */ (document.querySelector(`a[href^="${currentEpisode}"`));
     if (!thumbLink)
@@ -24,7 +25,13 @@
     const thumbSrc = thumbLink.querySelector('img').src;
     const bigSrc = thumbSrc.endsWith('/mainthumb.jpg') ? thumbSrc.replace('mainthumb.jpg', 'big.jpg') : null;
 
-    const useThumb = () => target.src = thumbSrc;
+    const useThumb = () => {
+      if (target.src === thumbSrc) {
+        target.src = originalSrc;
+        return target.removeEventListener('error', useThumb);
+      }
+      target.src = thumbSrc;
+    };
 
     target.addEventListener('error', useThumb);
     target.addEventListener('load', () => {
