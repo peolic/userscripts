edb13dabc07b78a19dbb3e994b5ae1990374a1c0
peolic <66393006+peolic@users.noreply.github.com>
2024-01-04 18:24:12 +0200

    mindgeek refactor

--- mindgeek-scene-trailer.user.js
+++ mindgeek-scene-trailer.user.js
@@ -1,11 +1,10 @@
 // ==UserScript==
 // @name        MindGeek Scene Trailer
 // @author      peolic
-// @version     0.4
+// @version     1.0
 // @description show trailers on MindGeek sites
 // @namespace   https://github.com/peolic
 // @match       http*://*/scene/*/*
-// @exclude     https://www.bellesafilms.com/*
 // @grant       none
 // @homepageURL https://github.com/peolic/userscripts
 // @downloadURL https://raw.githubusercontent.com/peolic/userscripts/main/mindgeek-scene-trailer.user.js
@@ -13,32 +12,67 @@
 // ==/UserScript==
 
 (async () => {
-  const wait = (/** @type {number} */ ms) => new Promise((resolve) => setTimeout(resolve, ms));
-  const getTitle = () => document.querySelector('h1')?.textContent || document.querySelector('h2')?.textContent || '_____';
-  const getImg = () => document.querySelector(`section img[src^="https://media-public-ht.project1content.com"][alt^="${getTitle()}"]`);
 
-  let img = getImg();
-  while (!img) {
-    img = getImg();
-    await wait(100);
-  }
+  const wait = (/** @type {number} */ ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+  const waitForLoad = async (postInit) => {
+    let timeout = 10000;
+
+    const getContentRoot = () => document.querySelector('#root > div[class]:first-of-type');
+    while (
+      !Object.getOwnPropertyNames(getContentRoot()).find((p) => /^__(reactFiber[a-z]?|reactInternalInstance)\$/.test(p))
+      || !document.querySelector('script[type="application/ld+json"]')
+    ) {
+      if (timeout <= 0) {
+        throw new Error('failed to initialize userscript: react search timed-out');
+      }
+      await wait(50);
+      timeout -= 50;
+    }
+
+    await wait(postInit);
+  };
+
+  await waitForLoad(0);
+
+  const dataJSONLD = document.querySelector('script[type="application/ld+json"]')?.textContent;
+  const data = JSON.parse(dataJSONLD);
+
+  const imgEl = document.querySelector(`img[src="${data.thumbnailUrl}"]`);
+
+  const imageLink = document.createElement('a');
+  imageLink.innerText = 'Image';
+  imageLink.href = imgEl.src;
+  imageLink.target = '_blank';
+  Object.assign(imageLink.style, {
+    position: 'absolute',
+    color: 'black',
+    top: '-22px',
+    right: '15px',
+  });
 
   const video = document.createElement('video');
   video.controls = true;
-  video.className = img.className;
-  video.poster = img.src;
+  video.className = imgEl.className;
+  video.poster = imgEl.src;
 
-  // poster to video:
-  // "https://media-public-ht.project1content.com/m=eaSaaTbWx/ee6/b6a/057/993/459/3b8/4d0/bc1/f32/d7b/7d/poster/poster_01.jpg"
-  // "https://prog-public-ht.project1content.com/ee6/b6a/057/993/459/3b8/4d0/bc1/f32/d7b/7d/mediabook/mediabook_720p.mp4"
-  video.src = img.src
-    .replace('/media-public-ht.', '/prog-public-ht.')
-    .replace(/(\.com\/)m=[a-zA-Z]+\//, '$1')
-    .replace(/\/poster\/poster_01\.jpg$/, '/mediabook/mediabook_720p.mp4');
+  if (data.contentUrl) {
+    video.src = data.contentUrl;
+  } else {
+    // poster to video:
+    // "https://media-public-ht.project1content.com/m=eaSaaTbWx/ee6/b6a/057/993/459/3b8/4d0/bc1/f32/d7b/7d/poster/poster_01.jpg"
+    // "https://prog-public-ht.project1content.com/ee6/b6a/057/993/459/3b8/4d0/bc1/f32/d7b/7d/mediabook/mediabook_720p.mp4"
+    video.src = imgEl.src
+      .replace('/media-public-ht.', '/prog-public-ht.')
+      .replace(/(\.com\/)m=[a-zA-Z]+\//, '$1')
+      .replace(/\/poster\/poster_01\.jpg$/, '/mediabook/mediabook_720p.mp4');
+  }
 
-  const imgParent = img.parentElement;
+  const imgParent = imgEl.parentElement;
   imgParent.appendChild(video);
   while (imgParent.childNodes.length && !imgParent.firstChild.isSameNode(video)) {
     imgParent.firstChild.remove();
   }
+  imgParent.prepend(imageLink);
+
 })();
